676f1a9e763819feac3f137aa4ccb35a6fd09a44
mehedycode <mehedycode@gmail.com>
2024-12-26 13:34:02 +0600

    Error Page Implement

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -11,6 +11,7 @@ import NeedVolunteerDetails from "../pages/NeedVolunteerDetails";
 import BeAVolunteer from "../pages/BeAVolunteer";
 import ManageMyPost from "../pages/ManageMyPost";
 import UpdateVolNeedPosts from "../pages/UpdateVolNeedPosts";
+import Error from "../pages/Error";
 
 
 
@@ -64,6 +65,10 @@ const router = createBrowserRouter([
   {
     path: 'volunteer/login',
     element: <Login/>
+  },
+  {
+    path: '*',
+    element: <Error />,
   },
  
  
